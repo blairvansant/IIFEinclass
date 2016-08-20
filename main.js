a71535063eca48d12769c9f7c6c9317aeba1d1fc
blairvansant <blairbot@gmail.com>
2016-08-20 10:08:43 -0500

    added examples of IIfe

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+	
+
+console.log ("Sedan IIFE", Sedan);
+console.log ("Sedan Color", Sedan.getColor());
+console.log ("Sedan type", Sedan.getType());
+
+console.log("Sedan set occupancy", Sedan.setOccupancy(6));
+console.log("Sedan set occupancy", Sedan.getOccupancy());
